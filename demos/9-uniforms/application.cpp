de2b6b121e47f91374de4a20775724c2d3bfc3ca
SpinnerX <heraaronhotmail@yahoo.com>
2025-08-01 02:08:27 -0700

    Changed filepath to shader to use sample 3

--- demos/9-uniforms/application.cpp
+++ demos/9-uniforms/application.cpp
@@ -337,11 +337,11 @@ main() {
 	// Now creating a vulkan graphics pipeline for the shader loading
 	std::array<vk::shader_source, 2> shader_sources = {
 		vk::shader_source{
-			.filename = "shader_samples/sample2/test.vert.spv",
+			.filename = "shader_samples/sample3/test.vert.spv",
 			.stage = vk::shader_stage::vertex
 		},
 		vk::shader_source{
-			.filename = "shader_samples/sample2/test.frag.spv",
+			.filename = "shader_samples/sample3/test.frag.spv",
 			.stage = vk::shader_stage::fragment
 		},
 	};
