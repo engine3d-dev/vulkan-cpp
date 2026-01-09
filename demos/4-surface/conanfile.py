0bd5df92d23ae2343cf15bb0688c5675c8bb09ec
SpinnerX <heraaronhotmail@yahoo.com>
2026-01-08 18:58:59 -0800

    Fixed vulkan-cpp cmake and conanfile to be consumed by external application and finalized more changes

--- demos/4-surface/conanfile.py
+++ demos/4-surface/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, cmake_layout
 
-class GameDemo(ConanFile):
+class Demo(ConanFile):
     name = "game-demo"
     version = "1.0"
     settings = "os", "compiler", "build_type", "arch"
@@ -10,16 +10,19 @@ class GameDemo(ConanFile):
 
     # Putting all of your build-related dependencies here
     def build_requirements(self):
-        self.tool_requires("make/4.4.1")
-        self.tool_requires("cmake/3.27.1")
+        self.tool_requires("cmake/[^4.0.0]")
+        self.tool_requires("ninja/[^1.3.0]")
         self.tool_requires("engine3d-cmake-utils/4.0")
 
     # Putting all of your packages here
     # To build engine3d/1.0 locally do the following:
     # conan create . --name=engine3d --version=0.1.0 --user=local --channel=12345
     def requirements(self):
-        self.requires("vulkan-headers/1.3.290.0")
-        self.requires("vulkan-cpp/1.0")
+        self.requires("glfw/3.4")
+        self.requires("glm/1.0.1")
+        self.requires("stb/cci.20230920")
+        self.requires("tinyobjloader/2.0.0-rc10")
+        self.requires("vulkan-cpp/2.0")
 
     def build(self):
         cmake = CMake(self)
